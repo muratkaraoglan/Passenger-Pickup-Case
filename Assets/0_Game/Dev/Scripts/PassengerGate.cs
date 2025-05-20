2a3bb8c37370a0098a5e175d716e88c378cf24a3
Murat Karaoglan <m.karaoglan1604@gmail.com>
2025-05-20 15:42:17 +0300

    Passanger system

---
+++ Assets/0_Game/Dev/Scripts/PassengerGate.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _0_Game.Dev.Scripts
+{
+    public class PassengerGate : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/0_Game/Dev/Scripts/Train/TrainManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _0_Game.Dev.Scripts.Train
+{
+    public class TrainManager : MonoBehaviour
+    {
+        
+    }
+}
